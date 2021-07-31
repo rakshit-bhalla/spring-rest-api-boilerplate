680da0b5b97103c73e37530d63f63a54c6f38f75
Rakshit Bhalla <rakshit.bhalla@yahoo.com>
2021-07-31 14:31:45 +0530

    first commit

---
+++ src/main/java/dev/rakshit/boilerplate/BoilerplateApplication.java
@@ -0,0 +1,13 @@
+package dev.rakshit.boilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateApplication.class, args);
+	}
+
+}
